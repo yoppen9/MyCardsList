4f5ee87160b45cc852f7030745d02e29ec918151
yoppen <yopppen.4403@gmail.com>
2021-07-09 20:29:04 +0900

    7/9製作途中

--- MyCardsList/CardsViewController.swift
+++ MyCardsList/CardsViewController.swift
@@ -7,33 +7,61 @@
 
 import UIKit
 
+var name: [String] = []
+var phoneNumber: [String] = []
+
 class CardsViewController: UIViewController {
     
-    @IBOutlet weak var cardsName: UILabel!
-    @IBOutlet weak var cardsImage: UIImageView!
-    @IBOutlet weak var cardsFace: UIImageView!
-    @IBOutlet weak var cardsDescription: UILabel!
-    @IBOutlet weak var cardsTel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if(UserDefaults.standard.array(forKey: "listPass") != nil) {
+            name = UserDefaults.standard.array(forKey: "listPass") as! [String]
+        }
+        if(UserDefaults.standard.array(forKey: "listPass2") != nil) {
+            phoneNumber = UserDefaults.standard.array(forKey: "listPass2") as! [String]
+        }
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        collectionView.collectionViewLayout = layout
     }
     @IBAction func settingButton(_ sender: Any) {
-        performSegue(withIdentifier: "settingSegue", sender: nil)
+        let nextView = storyboard?.instantiateViewController(identifier: "next") as! SettingViewController
+        nextView.presentationController?.delegate = self
+        present(nextView, animated: true, completion: nil)
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "settingSegue" {
-            let next = segue.destination as? SettingViewController
-            next?.resultHandler = { text in
-                self.cardsName.text = text
-            }
-            next?.resultHandler2 = { text in
-                self.cardsDescription.text = text
-            }
-            next?.resultHandler3 = { text in
-                
-                self.cardsTel.text = text
-            }
-        }
+}
+
+extension CardsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let nameCell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardsImage", for: indexPath)
+        let label = nameCell.contentView.viewWithTag(1) as! UILabel
+        let label2 = nameCell.contentView.viewWithTag(2) as! UILabel
+        label.text = name[indexPath.row]
+        label2.text = phoneNumber[indexPath.row]
+        return nameCell
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return name.count
+    }
+}
+
+extension CardsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let horizontalSpace:CGFloat = 20
+        let cellSize:CGFloat = self.view.bounds.width / 3 - horizontalSpace
+        let heightSize:CGFloat = cellSize * 1.5
+        return CGSize(width: cellSize, height: heightSize)
+    }
+}
+
+extension CardsViewController: UIAdaptivePresentationControllerDelegate {
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        collectionView.reloadData()
     }
 }
--- MyCardsList/SettingViewController.swift
+++ MyCardsList/SettingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate, UITextViewDelegate {
+class SettingViewController: UIViewController,UINavigationControllerDelegate,UITextViewDelegate {
     
     @IBOutlet weak var NewCardsName: UITextField!
     @IBOutlet weak var NewCardsImage: UIImageView!
@@ -24,27 +24,22 @@ class SettingViewController: UIViewController,UIImagePickerControllerDelegate,UI
         self.NewCardsName.delegate = self
         self.NewCardsDescription.delegate = self
         self.NewCardsTel.delegate = self
-        
 //        電話番号入力を数字だけにする
         self.NewCardsTel.keyboardType = UIKeyboardType.numberPad
     }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if (self.NewCardsTel.isFirstResponder) {
             self.NewCardsTel.resignFirstResponder()
         }
     }
-    
     func textFieldShouldReturn(_ NewCardsName: UITextField) -> Bool {
         NewCardsName.resignFirstResponder()
         return true
     }
-
     func textFieldShouldReturn2(_ NewCardsTel: UITextField) -> Bool {
         NewCardsTel.resignFirstResponder()
         return true
     }
-    
     @IBAction func ImageButtonAction(_ sender: Any) {
         let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
@@ -70,36 +65,52 @@ class SettingViewController: UIViewController,UIImagePickerControllerDelegate,UI
     
         present(alertController, animated: true, completion: nil)
     }
-    
     @IBAction func NewCardsButton(_ sender: Any) {
-        guard let text = self.NewCardsName.text else { return }
-        if let handler = self.resultHandler {
-            handler(text)
-        }
-        guard let text = self.NewCardsDescription.text else { return }
-        if let handler = self.resultHandler2 {
-            handler(text)
-        }
-        guard let text = self.NewCardsTel.text else { return }
-        if let handler = self.resultHandler3 {
-            handler(text)
-        }
-        self.dismiss(animated: true, completion: nil)
-//        UserDefaults
-    }
-//    カメラ撮影、フォトライブラリーの選択後に呼び出される
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//        撮影、ライブラリーから選択した画像をNewCardsImageに配置
-        NewCardsImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-//        モーダルビューを閉じる
-        dismiss(animated: true, completion: nil)
-    }
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//
-//        if let searchKey = textField.text {
-//            print(searchKey)
+        guard let text = NewCardsName.text, !text.isEmpty else { return }
+        guard let text = NewCardsTel.text, !text.isEmpty else { return }
+        name.append(NewCardsName.text!)
+        phoneNumber.append(NewCardsTel.text!)
+        print(name)
+        print(phoneNumber)
+        NewCardsName.text! = ""
+        NewCardsTel.text! = ""
+        UserDefaults.standard.set(name, forKey: "listPass")
+        UserDefaults.standard.set(phoneNumber, forKey: "listPass2")
+//        guard let text = self.NewCardsName.text else { return }
+//        if let handler = self.resultHandler {
+//            handler(text)
 //        }
-//        return true
-//    }
+//        guard let text = self.NewCardsDescription.text else { return }
+//        if let handler = self.resultHandler2 {
+//            handler(text)
+//        }
+//        guard let text = self.NewCardsTel.text else { return }
+//        if let handler = self.resultHandler3 {
+//            handler(text)
+//        }
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SettingViewController: UIImagePickerControllerDelegate {
+    //    カメラ撮影、フォトライブラリーの選択後に呼び出される
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    //        撮影、ライブラリーから選択した画像をNewCardsImageに配置
+            NewCardsImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+    //        モーダルビューを閉じる
+            dismiss(animated: true, completion: nil)
+        }
+}
+
+extension SettingViewController {
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        super.dismiss(animated: flag, completion: completion)
+        guard let presentationController = presentationController else {
+            return
+        }
+        presentationController.delegate?.presentationControllerDidDismiss?(presentationController)
+    }
+}
+
+extension SettingViewController: UITextFieldDelegate {
 }
